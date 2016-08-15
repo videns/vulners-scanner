d57922d8b7147b134486d55032276ce261b21634
videns <ibulatenko@gmail.com>
2016-08-15 21:50:44 +0300

    README modification and lazyScanner v0.1

--- README.md
+++ README.md
@@ -1,1 +1,76 @@
 # vulners-scanner
+# Desciption
+PoC of a host-based vulnerability scanner, which uses vulners.com API. Detects operating system, collect installed packages and check vulnerabilities in it.
+# Supported OS
+Currently support collecting packages for these operating systems:
+* Debian-based (debian, kali, ubuntu)
+* Rhel-based (redhat, centos, fedora)
+# Use
+
+# Docker support
+Experimental support of detecting vulnerabilities in running docker containters
+
+
+# How to use
+* Lazy scanner
+The simplest script to show vulners API capabilities. Just run script and it will return all found vulnerabilities:
+```
+#python2.6 lazyScanner.py
+OS Name - debian, OS Version - 8
+Total provided packages: 315
+{
+    "data": {
+        "vulnerabilities": [
+            "DSA-3644",
+            "DSA-3626"
+        ],
+        "packages": {            
+            "openssh-client 1:6.7p1-5+deb8u2 amd64": {
+                "DSA-3626": [
+                    {
+                        "bulletinVersion": "1:6.7p1-5+deb8u3",
+                        "providedVersion": "1:6.7p1-5+deb8u2",
+                        "bulletinPackage": "openssh-client_1:6.7p1-5+deb8u3_all.deb",
+                        "result": true,
+                        "operator": "lt",
+                        "OSVersion": "8",
+                        "providedPackage": "openssh-client 1:6.7p1-5+deb8u2 amd64"
+                    }
+                ]
+            }
+            "fontconfig-config 2.11.0-6.3 all": {
+                "DSA-3644": [
+                    {
+                        "bulletinVersion": "2.11.0-6.3+deb8u1",
+                        "providedVersion": "2.11.0-6.3",
+                        "bulletinPackage": "fontconfig-config_2.11.0-6.3+deb8u1_all.deb",
+                        "result": true,
+                        "operator": "lt",
+                        "OSVersion": "8",
+                        "providedPackage": "fontconfig-config 2.11.0-6.3 all"
+                    }
+                ]
+            },
+            "libfontconfig1 2.11.0-6.3 amd64": {
+                "DSA-3644": [
+                    {
+                        "bulletinVersion": "2.11.0-6.3+deb8u1",
+                        "providedVersion": "2.11.0-6.3",
+                        "bulletinPackage": "libfontconfig1_2.11.0-6.3+deb8u1_all.deb",
+                        "result": true,
+                        "operator": "lt",
+                        "OSVersion": "8",
+                        "providedPackage": "libfontconfig1 2.11.0-6.3 amd64"
+                    }
+                ]
+            }
+        }
+    },
+    "result": "OK"
+}
+Vulnerabilities:
+DSA-3644
+DSA-3626
+```
+
+
---
+++ lazyScanner.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+__author__ = 'videns'
+try:
+    import urllib.request as urllib2
+except ImportError:
+    import urllib2
+try:
+    from subprocess import DEVNULL # py3k
+except ImportError:
+    import os
+    DEVNULL = open(os.devnull, 'wb')
+import json
+import re
+import subprocess
+
+VULNERS_LINKS = {'pkgChecker':'https://vulners.com/api/v3/audit/audit/',
+                 'bulletin':'https://vulners.com/api/v3/search/id/?id=%s'}
+
+
+class LazyScanner():
+    def __init__(self):
+        pass
+
+    def sshCommand(self,cmd):
+        cmdResult = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=DEVNULL, shell=True).communicate()[0]
+        if isinstance(cmdResult, bytes):
+            cmdResult = cmdResult.decode('utf8')
+        return cmdResult
+
+
+
+    def getOSInfo(self):
+        version = self.sshCommand("cat /etc/os-release")
+        if version:
+            osFamily = re.search("^ID=\"?(\w+)\"?",version,re.MULTILINE).group(1)
+            osVersion = re.search("^VERSION_ID=\"?(\w+)\"?",version,re.MULTILINE).group(1)
+            return (osFamily, osVersion)
+
+    def getPackages(self, osName):
+        if osName in ('debian','ubuntu', 'kali'):
+            cmd = "dpkg-query -W -f='${Package} ${Version} ${Architecture}\n'"
+        elif osName in ('redhat', 'centos', 'oraclelinux', 'suse', 'fedora'):
+            cmd = "rpm -qa"
+        else:
+            cmd = None
+        return self.sshCommand(cmd).splitlines() if cmd else None
+
+
+    def auditSystem(self):
+        osInfo = self.getOSInfo()
+        if not osInfo:
+            print("Couldn't detect OS")
+            return
+        print("OS Name - %s, OS Version - %s" % (osInfo[0], osInfo[1]))
+
+        installedPackages = self.getPackages(osInfo[0])
+        if not installedPackages:
+            print("Couldn't find packages")
+            return
+
+        print("Total provided packages: %s" % len(installedPackages))
+        # Get vulnerability information
+        payload = {'os':osInfo[0],
+                   'version':osInfo[1],
+                   'package':installedPackages}
+        req = urllib2.Request(VULNERS_LINKS.get('pkgChecker'))
+        req.add_header('Content-Type', 'application/json')
+        response = urllib2.urlopen(req, json.dumps(payload).encode('utf-8'))
+        responseData = response.read()
+        if isinstance(responseData, bytes):
+            responseData = responseData.decode('utf8')
+        responseData = json.loads(responseData)
+        resultCode = responseData.get("result")
+        if resultCode == "OK":
+            print(json.dumps(responseData, indent=4))
+            print("Vulnerabilities:\n%s" % "\n".join(responseData.get('data').get('vulnerabilities')))
+        else:
+            print("Error - %s" % responseData.get('data').get('error'))
+        return
+
+if __name__ == "__main__":
+    scanner = LazyScanner()
+    scanner.auditSystem()
